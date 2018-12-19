136d2c1401d8f084a9e842a86295d623b02ab8d0
Adan Constanzo <aconstanzolan@gmail.com>
2018-12-19 10:25:40 -0800

    Added message when on my_decks and have no decks. Issue can be found here: https://github.com/AdanConstanzo/react-ptcg/issues/12

--- src/components/pages/MyDecksPage/index.js
+++ src/components/pages/MyDecksPage/index.js
@@ -1,34 +1,47 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Message} from 'semantic-ui-react';
 
 import api from '../../../api'
 import DeckNav from './DeckNav'; 
 
 class index extends React.Component {
     state = {
-        loading: true,
+        loading: undefined,
         decks: []
     };
 
     componentDidMount() {
         api.deck.GetLoginDeck()
-            .then(decks => this.setState({ decks, loading: false }));
+            .then(decks => decks.length > 0 ? this.setState({ decks, loading: true }) : this.setState({ loading: false }));
     }
 
     render(){
-        const { decks } = this.state
-        return (
-                <Card.Group itemsPerRow={6} stackable >
-                    {decks.length > 0 && (
-                        decks.map((deck) =>
-                            <DeckNav
-                                key={deck._id}
-                                deck={deck}
-                                info={true}
-                            />)
-                    )}
-                </Card.Group>
-        )
+				const { decks, loading } = this.state
+				if (loading === false) {
+					return (
+						<Message>
+							<Message.Header>Seems like your deck page is a little empty!</Message.Header>
+							<a href="deck_builder" ><p>You can start to create some decks here!</p></a>
+						</Message>
+					)
+				} else if (loading === true) {
+					return (
+						<Card.Group itemsPerRow={6} stackable >
+								{decks.length > 0 && (
+										decks.map((deck) =>
+												<DeckNav
+														key={deck._id}
+														deck={deck}
+														info={true}
+												/>)
+								)}
+						</Card.Group>
+					)
+				}
+				return(
+					<div></div>
+				)
+        
     }
 }
 export default index;
